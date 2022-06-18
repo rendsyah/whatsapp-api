52c45f374319659b2506a96de17d6a6d83e66aee
Rendy F <rendy@missiidea.com>
2022-06-18 14:41:30 +0700

    remove optional param

--- src/services/auth/authWA.ts
+++ src/services/auth/authWA.ts
@@ -1,6 +1,6 @@
 import { LocalAuth } from "whatsapp-web.js";
 
-export const authWA = (sessionClient: string, sessionPath?: string) => {
+export const authWA = (sessionClient: string, sessionPath: string) => {
     return new LocalAuth({
         clientId: sessionClient,
         dataPath: sessionPath,
