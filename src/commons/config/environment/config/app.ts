20172116c856fe95bb01787b7a52b2574bc12ed6
Rendy F <rendy@missiidea.com>
2023-02-27 08:15:18 +0700

    add crypto prefix app

--- src/commons/config/environment/config/app.ts
+++ src/commons/config/environment/config/app.ts
@@ -3,6 +3,7 @@ import { registerAs } from '@nestjs/config';
 
 // Define Prefix Config App
 export default registerAs('app', () => ({
+    SERVICE_NODE_ENV: process.env.NODE_ENV,
     SERVICE_PORT: process.env.SERVICE_PORT,
     SERVICE_NAME: process.env.SERVICE_NAME,
     SERVICE_PREFIX: process.env.SERVICE_PREFIX,
@@ -11,4 +12,7 @@ export default registerAs('app', () => ({
     SERVICE_JWT_EXPIRES_IN: process.env.SERVICE_JWT_EXPIRES_IN,
     SERVICE_JWT_REFRESH_SECRET_KEY: process.env.SERVICE_JWT_REFRESH_SECRET_KEY,
     SERVICE_JWT_REFRESH_EXPIRES_IN: process.env.SERVICE_JWT_REFRESH_EXPIRES_IN,
+    SERVICE_CRYPTO_ALGORITHM: process.env.SERVICE_CRYPTO_ALGORITHM,
+    SERVICE_CRYPTO_SECRET_KEY: process.env.SERVICE_CRYPTO_SECRET_KEY,
+    SERVICE_UPLOAD_PATH: process.env.SERVICE_UPLOAD_PATH,
 }));
