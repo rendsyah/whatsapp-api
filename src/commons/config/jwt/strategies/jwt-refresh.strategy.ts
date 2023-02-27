942f72b5186b9380a2026c4b27d6d22237857c62
Rendy F <rendy@missiidea.com>
2023-02-27 08:15:54 +0700

    change logic refresh strategy

--- src/commons/config/jwt/strategies/jwt-refresh.strategy.ts
+++ src/commons/config/jwt/strategies/jwt-refresh.strategy.ts
@@ -5,31 +5,43 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
 
+// Import Commons
+import { ApiUnauthorizedException } from '@commons/exception/api-exception';
+
+// Import Datasource
+import { ProjectDbService } from '@datasource/project-db/project-db.service';
+
 // Import Interfaces
-import { IPayload } from './interfaces/jwt.interface';
+import { IAuthTokens } from '@modules/auth/interfaces/auth.interface';
+import { IGetProjectDbModels } from '@datasource/interfaces/project-db.interface';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor(configService: ConfigService) {
+    private projectDbModels: IGetProjectDbModels;
+
+    constructor(configService: ConfigService, private readonly projectDbService: ProjectDbService) {
         super({
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => {
-                    return request.headers?.authentication?.toString() || null;
-                },
-            ]),
+            jwtFromRequest: ExtractJwt.fromExtractors([(request: Request) => request.headers?.authorization]),
             ignoreExpiration: false,
             secretOrKey: configService.get('app.SERVICE_JWT_REFRESH_SECRET_KEY'),
             passReqToCallback: true,
         });
+        this.projectDbModels = this.projectDbService.getModels();
     }
 
-    async validate(request: Request, payload: IPayload) {
-        const refreshToken = request.get('authentication');
+    async validate(request: Request, payload: IAuthTokens) {
+        const getUser = await this.projectDbModels.UsersModels.findOne({ where: { id: payload.sub } });
+        const getRefreshToken = request.get('authorization');
+        const getHashToken = getUser.hash_token;
+
+        if (!getHashToken) {
+            throw new ApiUnauthorizedException('invalid token');
+        }
+
         return {
             userId: payload.sub,
             username: payload.username,
-            role: payload.role,
-            refreshToken,
+            refreshToken: getRefreshToken,
         };
     }
 }
